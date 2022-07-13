3d5846996a598673e6b4e3dd94909e091983978f
Daniele Baggio <dbaggio@tierratelematics.com>
2022-07-13 16:23:30 +0200

    test entrypoint

--- .github/actions/ansible/entrypoint.sh
+++ .github/actions/ansible/entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-echo "$VAULT_PASS" > ~/.vault_pass.txt
-echo "ANSIBLE"
-mkdir ~/.ssh
-ansible-vault --vault-password-file ~/.vault_pass.txt view ansible/ssh_key.txt.secret > ~/.ssh/id_rsa
-chmod 0600 ~/.ssh/id_rsa
+# echo "$VAULT_PASS" > ~/.vault_pass.txt
+# echo "ANSIBLE"
+# mkdir ~/.ssh
+# ansible-vault --vault-password-file ~/.vault_pass.txt view ansible/ssh_key.txt.secret > ~/.ssh/id_rsa
+# chmod 0600 ~/.ssh/id_rsa
 
-ansible-playbook -e "build_sha=$GITHUB_SHA" --vault-password-file ~/.vault_pass.txt -i ansible/hosts ansible/ansible.yaml
+ansible-playbook -e "build_sha=$GITHUB_SHA" ansible/ansible.yaml
